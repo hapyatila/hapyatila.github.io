73fcd69e79e8838fdfda92954c636e2a1769deb8
hapyatila <31728582+hapyatila@users.noreply.github.com>
2025-12-18 12:54:27 -0500

    simplification

---
+++ log_MaMaExpress.js
@@ -0,0 +1,6 @@
+// Fichier généré automatiquement - Ne pas modifier manuellement
+// Source: Logs d'utilisation MaMaExpress
+// Ce fichier sera mis à jour automatiquement par MaMaExpress.html
+
+const MAMAMA_USAGE_LOGS = [];
+
